86c602e61a416fbb658cc91120760b3417dd0c8e
Victor Durand <v.durand@piman-consultants.fr>
2022-07-13 18:29:12 +0400

    feat: boostrap cdn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: "https://api-dev.master-classroom.com/api",
+  mapboxAccessToken : "pk.eyJ1IjoidmljdG9yZHJuZCIsImEiOiJjanR3eHhhY3oxNDUwNDNsemE1aG5peGl2In0.YeRJsFQXOp8GFHBiQsoHEQ",
   publicStripeKey : "pk_test_EgijVNj7OVzXPr5onG0G31Zo",
   chatServer : "https://api-tchat-dev.master-classroom.com",
   socketServer : "https://socket.master-classroom.com"
